c6413fe8c00dedda86190014dfe49b487e7d1165
Jacob Scott <jscott2718@gmail.com>
2025-03-09 21:18:46 +0000

    fix: use local M instead of M

--- lua/ark/init.lua
+++ lua/ark/init.lua
@@ -1,4 +1,4 @@
-M = {}
+local M = {}
 
 local helper_file = function(file)
     local curr_file = debug.getinfo(1).source:gsub("^@", "")
@@ -103,7 +103,7 @@ end
 
 ---@param opts? table Passed to |nvim_open_win()|
 M.open = function(opts)
-    if not require("ark").is_running() then
+    if not M.is_running() then
         M.start_kernel()
     end
 
@@ -168,16 +168,16 @@ function M.setup(cfg)
         config.log_file = vim.fn.tempname() .. "ark.log"
     end
 
-    vim.api.nvim_create_user_command("ArkStartKernel", function() require("ark").start_kernel() end, {})
-    vim.api.nvim_create_user_command("ArkOpen",        function() require("ark").open() end, {})
-    vim.api.nvim_create_user_command("ArkKill",        function() require("ark").kill() end, {})
-    vim.api.nvim_create_user_command("ArkRestart",     function() require("ark").restart() end, {})
+    vim.api.nvim_create_user_command("ArkStartKernel", function() M.start_kernel() end, {})
+    vim.api.nvim_create_user_command("ArkOpen",        function() M.open() end, {})
+    vim.api.nvim_create_user_command("ArkKill",        function() M.kill() end, {})
+    vim.api.nvim_create_user_command("ArkRestart",     function() M.restart() end, {})
 
     if config.auto_start then
         vim.api.nvim_create_autocmd("BufEnter", {
             pattern = "*.R",
             group = vim.api.nvim_create_augroup("ark", {}),
-            callback = function() require("ark").start_lsp() end,
+            callback = function() M.start_lsp() end,
         })
     end
 end
